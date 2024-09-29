fa91a2f92da8be8dffc2908fa8d68832867e8f55
saeed.rafieepour@gmail.com <saeed.rafieepour@gmail.com>
2024-09-28 22:04:27 -0400

    setup.py

---
+++ setup.py
@@ -0,0 +1,30 @@
+from setuptools import setup, find_packages
+from typing import List
+
+HYPHENE_E_DOT = "-e ."
+def get_requirements(file_path:str) -> List[str]:
+    '''
+    This functions returns a list of packages to install for this project
+    '''
+    requirements=[]
+    with open(file_path) as file_obj:
+        requirements = file_obj.readlines()
+
+        requirements = [req.replace("\n","") for req in requirements]
+
+        if HYPHENE_E_DOT in requirements:
+            requirements.remove(HYPHENE_E_DOT)
+
+    return requirements
+
+setup(
+    name = 'mlproject',
+    version = '0.0.1',
+    author = 'Saeed',
+    author_email='saeed@gmail.com',
+    packages=find_packages(),
+    install_requires=get_requirements('requirements.txt')
+
+
+
+)
