8b62482212cd8f62059f34a9cc3a85b4741b8dbd
saeed.rafieepour@gmail.com <saeed.rafieepour@gmail.com>
2024-10-14 22:22:15 -0400

    Hyper-parameter training

--- src/utils.py
+++ src/utils.py
@@ -8,6 +8,7 @@ from src.exception import CustomException
 from src.logger import logging
 import dill
 from sklearn.metrics import r2_score
+from sklearn.model_selection import GridSearchCV
 
 def save_object(file_path, obj):
 
@@ -22,14 +23,21 @@ def save_object(file_path, obj):
     except Exception as e:
         raise CustomException(e,sys)
     
-def evaluate_models(X_train, y_train, X_test, y_test,
-                                               models):
-    
+def evaluate_models(X_train,y_train,X_test,y_test,models,param,cv=3,n_jobs=3,verbose=1,refit=False):
+    cv=3
     try:
         report = {}
         for i in range(len(list(models))):
             model = list(models.values())[i]
-            model.fit(X_train, y_train)  # train the model
+            para = param[list(models.keys())[i]]
+
+            gs = GridSearchCV(model, para, cv=cv, n_jobs=n_jobs, verbose=verbose, refit=refit)
+            gs.fit(X_train, y_train)  # train the model
+
+            model.set_params(**gs.best_params_)
+            model.fit(X_train, y_train) 
+
+            # model.fit(X_train, y_train)  # train the model
             y_train_pred = model.predict(X_train)
             y_test_pred = model.predict(X_test)
 
